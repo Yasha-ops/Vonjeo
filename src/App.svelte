d3e474dcbbdf53abc5dc977cea35c56e3a7dc87c
Karim.abou-dargham <karim.abou-dargham@epita.fr>
2022-06-25 19:48:03 +0200

    feat(frontend): just did the bare minium to have a spotify sidebar

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,7 @@
     import SideBar from './SideBar/SideBar.svelte'
     import DisableScrollBar from './DisableScrollBar.svelte';
     import MainPage from './MainPage/MainPage.svelte';
-    
+    import SpotifyDrawer from './Drawer/SpotifyDrawer.svelte';
     import SearchDrawer from './Drawer/SearchDrawer.svelte';
     import Drawer from './Drawer/Drawer.svelte';
 
@@ -19,6 +19,7 @@
     <Drawer drawer_id={"drawer-files"} name={"Files"}/>
     
     <SearchDrawer/>
+    <SpotifyDrawer/>
 
     <MainPage/>
 </div>
--- src/SideBar/SideBar.svelte
+++ src/SideBar/SideBar.svelte
@@ -5,10 +5,13 @@
     import BsFiles from "svelte-icons-pack/bs/BsFiles";
     import BsSearch from "svelte-icons-pack/bs/BsSearch";
     import BsBug from "svelte-icons-pack/bs/BsBug";
+    import ImSpotify from "svelte-icons-pack/im/ImSpotify";
+
 </script>
 
 <div class="flex fixed top-0 left-0 h-screen w-14 m-0 flex-col bg-zinc-700">
     <SideBarIcons icon={BsFiles} drawer_id="drawer-files"/>
     <SideBarIcons icon={BsSearch} drawer_id="drawer-search"/>
+    <SideBarIcons icon={ImSpotify} drawer_id="drawer-spotify"/>
     <SideBarIcons icon={BsBug}/>
 </div>
