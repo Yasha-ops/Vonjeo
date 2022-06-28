8b76259b33c43afc0e80733c3164c071287b07ee
Karim.abou-dargham <karim.abou-dargham@epita.fr>
2022-06-28 19:52:14 +0200

    feat(frontend): removed dead code

---
+++ src/Drawer/SpotifyDrawer.svelte
@@ -0,0 +1,5 @@
+<div class="flex-none drawer flex" id="drawer-spotify" style="display: none;">
+    <div class="relative flex w-auto h-9 items-center">
+        <p class="text-xs text-white uppercase ml-3">Spotify</p>
+    </div>
+</div>
