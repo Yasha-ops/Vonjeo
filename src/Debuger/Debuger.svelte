f9b06da28a989bd39ec46a47a572c73362b775d7
Eliott Beguet <eliott.beguet@epita.fr>
2022-07-01 05:40:13 +0200

    feqt(debuger): better ui and basic button

--- src/Debuger/Debuger.svelte
+++ src/Debuger/Debuger.svelte
@@ -1,12 +1,11 @@
 <script>
-    import Flex from 'svelte-flex';
     import { onDestroy } from 'svelte';
     import { HSplitPane } from 'svelte-split-pane';
 
     import { launchServer, PythonDebug } from '../lib/callPythonDebuger'
     import { debug_on } from '../lib/Store'
 
-    const pd = new PythonDebug('/home/geox/ing1/tlya/tp1/a.out');
+    const pd = new PythonDebug('./a.out');
     let promise = null;
 
     async function handleClick() {
@@ -41,26 +40,18 @@
             stopServer();
     });
 
-    const codes = pd.lines();
+    let codes = pd.lines();
+    let act = false;
+    let files;
 
-    let w, h;
+    $: if (files) {
+        codes = pd.file();
+    }
+
+    // suposed to updates text in top div of the page but idk y it doesn't
+    // for ex when i click on run it's  not updating D:
+    $: act, codes = pd.file();
 </script>
-
-
-<div class="flex-none w-full h-auto">
-    <button on:click={handleClick}>
-    	generate random number
-    </button>
-
-    {#await promise}
-    	<p>...waiting</p>
-    {:then number}
-    	<p>The number is {number}</p>
-    {:catch error}
-    	<p style="color: red">{error.message}</p>
-    {/await}
-</div>
-
 
 <!--Flex direction="column" align="center" justify="evenly" bind:clientWidth={w} bind:clientHeight={h}>
     <div class="inside" height={h / 3}>
@@ -84,11 +75,17 @@
 	</SplitPane>
 </Flex-->
 
-
-
-<div class="flex flex-col flex-auto h-full w-full bg-green-500">
+<div class="flex flex-col flex-auto h-full w-full">
     <div class="inside flex-auto">
-        NON
+        {#await codes}
+            <p>Loading ...</p>
+        {:then codes}
+            {#each pd.toLines(codes) as line}
+                <p>{line}</p>
+            {/each}
+        {:catch error}
+            <p style="color: red">{error.message}</p>
+        {/await}
     </div>
     
     <div class="flex-auto">
@@ -98,7 +95,18 @@
             </div>
     
             <div id="red" slot="right">
-                Right
+                <div class="btn-group">
+                    <input type="file" bind:files>
+                    <button on:click={async () => { codes = pd.toLines(await pd.continue()); act = !act; }}>
+                        Continue
+                    </button>
+                    <button on:click={async () => { codes = pd.toLines(await pd.run()); act = !act; }}>
+                        Run
+                    </button>
+                    <button on:click={async () => { codes = pd.toLines(await pd.run()); act = !act; }}>
+                        Run2
+                    </button>
+                </div>
             </div>
         </HSplitPane>
     </div>
@@ -111,5 +119,22 @@
         border-color: rgb(83, 177, 156);
         border-width: 4px;
         background-color: rgb(39 39 42 / var(--tw-bg-opacity));
+    }
+
+    .btn-group button {
+        background-color: #4c63af86; /* Green */
+        border: none;
+        color: white;
+        padding: 15px 32px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        cursor: pointer;
+        float: left;
+    }
+
+    .btn-group button:hover {
+        background-color: #36378f57;
     }
 </style>
--- src/lib/callPythonDebuger.js
+++ src/lib/callPythonDebuger.js
@@ -1,3 +1,4 @@
+import { component_subscribe } from "svelte/internal";
 
 const port = 5555;
 const backend_port = 8000;
@@ -71,5 +72,31 @@ export class PythonDebug {
 
     async exit() {
         return await getFetchContent('http://localhost:' + port + "/exit");
+    }
+
+    toLines(input) {
+        console.log("input:", input);
+        const data = JSON.parse(input);
+        
+        data.shift();
+        return data.map(o => {
+            if (o['stream'] !== 'stdout') {
+                console.log(o);
+            }
+            else {
+                let res = "";
+
+                if (o['message']) {
+                    res += o['message'] + " ";
+                }
+                if (o['payload']) {
+                    res += (typeof o['payload'] === "object") ?
+                        o['payload']['msg'] :
+                        o['payload'];
+                }
+
+                return res;
+            }
+        });
     }
 }
