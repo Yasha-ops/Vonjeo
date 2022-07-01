f60b2b1d1b06efe2dd0689c713e3f4b524ae027b
Karim.abou-dargham <karim.abou-dargham@epita.fr>
2022-07-01 10:20:19 +0200

    fix(frontend): spotify server and client favicon issue resolved, build issue to discuss

--- src/SideBar/SideBar.svelte
+++ src/SideBar/SideBar.svelte
@@ -10,6 +10,7 @@
     import BsFiles from "svelte-icons-pack/bs/BsFiles";
     import BsSearch from "svelte-icons-pack/bs/BsSearch";
     import BsBug from "svelte-icons-pack/bs/BsBug";
+    import BsSpotify from "svelte-icons-pack/bs/BsSpotify";
 
     // Functions
     const launchDebugger = () => {
@@ -21,14 +22,52 @@
         }];
     }
 
+    const launchSpotify = () => {
+        const { exec } = require('child_process');
+
+        exec('cd ~/src/spotify/server; npm start', (error, stdout, stderr) => {
+
+
+          if (error) {
+            console.error(`error: ${error.message}`);
+            return;
+          }
+      
+          if (stderr) {
+            console.error(`stderr: ${stderr}`);
+            return;
+          }
+
+        });
+        exec('cd ~/src/spotify/client; npm start', (error, stdout, stderr) => {
+
+
+        if (error) {
+          console.error(`error: ${error.message}`);
+          return;
+        }
+
+        if (stderr) {
+          console.error(`stderr: ${stderr}`);
+          return;
+        }
+
+        });
+    }
+
 </script>
 
 <div class="flex fixed top-0 left-0 h-screen w-14 m-0 flex-col bg-zinc-700">
-    <SideBarIcons icon={BsFiles} drawer_id="drawer-files"/>
-    <SideBarIcons icon={BsSearch} drawer_id="drawer-search"/>
+    <SideBarIcons icon={BsFiles} drawer_id="drawer-files" />
+    <SideBarIcons icon={BsSearch} drawer_id="drawer-search" />
 
     <!-- Debug butt on -->
     <div class="sidebar-icon" on:click={launchDebugger}>
-        <Icon src={BsBug} size="20"/>
+        <Icon src={BsBug} size="20" />
+    </div>
+
+    <!-- Spotify subproccess launcher-->
+    <div class="sidebar-icon" on:click={launchSpotify}>
+        <Icon src={BsSpotify} size="20" />
     </div>
 </div>
---
+++ src/SpotifyApp.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { WebPlayback } from 'svelte-spotify-web-playback';
+  import CustomPlayer from './src/my_custom_player.svelte';
+  const client_id = 'your_client_id';
+  let wrapper; // Reference to our wrapper element.
+</script>
+
+<WebPlayback 
+  {client_id} 
+  name="My Spotify Player" 
+  volume={0.5} 
+  bind:this={wrapper}
+>
+  <!-- Direct usage -->
+  <div slot="login">
+      <button on:click={() => wrapper.login()}>Login with Spotify</button>
+  </div>
+  <!-- Custom element usage -->
+  <CustomPlayer slot="player" let:player let:state {player} {state} />
+</WebPlayback>
