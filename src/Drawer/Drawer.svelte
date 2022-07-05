da0bc60bd7dee119f0c72660c828a17cb5045ed5
Raphaël Duhen <raphael.duhen.nexus@gmail.com>
2022-07-06 00:35:25 +0200

    feat(tree): add working tree, missing icons

--- src/Drawer/Drawer.svelte
+++ src/Drawer/Drawer.svelte
@@ -3,9 +3,11 @@
     export let drawer_id;
 </script>
 
-<div class="drawer flex-none" id={drawer_id} style="display: none;">
-    <div class="relative flex w-auto h-9 items-center">
-        <p class="text-xs text-white uppercase ml-3">{name}</p>
+<div class="flex-none drawer overflow-auto" id={drawer_id} style="display: none;">
+    <div class="flex relative items-center w-auto h-9">
+        <p class="ml-3 text-xs text-white uppercase">{name}</p>
+    </div>
+    <div class="px-2">
         <slot></slot>
     </div>
 </div>
--- src/Utils/store.js
+++ src/Utils/store.js
@@ -6,9 +6,9 @@ export const TypeFile = Object.freeze({
 });
 
 export const store_tabs = writable([
-	{type: TypeFile.FILE, filename:"Orange", content:"ORANGE", id:"panel-Orange"},
-	{type: TypeFile.FILE, filename:"Pineapple",content:"PINEAPPLE", id:"panel-Pineapple"},
-	{type: TypeFile.FILE, filename:"Peach", content:"PEACH", id:"panel-Peach"}
+    { type: TypeFile.FILE, filename: "Orange", content: "ORANGE", id: "panel-Orange" },
+    { type: TypeFile.FILE, filename: "Pineapple", content: "PINEAPPLE", id: "panel-Pineapple" },
+    { type: TypeFile.FILE, filename: "Peach", content: "PEACH", id: "panel-Peach" }
 ]);
 
 export const tabs = writable([]);
@@ -18,8 +18,8 @@ export const panels = [];
 
 
 export const logger = (type) => {
-    return function(line){
-        return function(message){
+    return function (line) {
+        return function (message) {
             return `[${type}][${line}]: ${message}`
         }
     }
@@ -30,3 +30,8 @@ export const ERROR = logger("ERROR");
 export const DEBUG = logger("DEBUG");
 
 export let nbr_screens = writable(0);
+
+
+export let file_tree = fetch('http://localhost:8000/project?dir=src')
+    .then((response) => response.json())
+    .catch(err => console.error(err));
